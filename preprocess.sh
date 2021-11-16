6cc0761da021530be7dddd161b2f30d447e2883f
Cameron Holdaway <cameron.holdaway@gmail.com>
2021-11-16 10:04:51 -0800

    finalized preprocessing

--- preprocess.sh
+++ preprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #loop through lines of full month file and extract subreddits with counts
-# loop through subreddit-count file to trim/extractwords from subreddits with at least N comments
-# Loop through words and 
+# loop through subreddit-count file to trim/extract words from subreddits with at least N comments
+# Parse the words 
 # input is file of all comments in a month, and limit for min comments in a reddit and min word count
 	#$1 = the full month of comments
 	#$2 = the min comment count for a sub in that month
@@ -20,10 +20,10 @@ do
        		break
 	fi
 
-	
-	cat $1 |jq 'select(.subreddit=='"$sub"') | .body' | tr '[:space:]' '[\n*]' | tr -d '[:punct:]'| tr [:upper:] [:lower:] | grep -v "^\s*$" | sort | uniq -c | sort -bnr | awk 'int($1)>=$3' > word_count/test.txt 
+	cat $1 |jq 'select(.subreddit=='"$sub"') | .body' | tr '[:space:]' '[\n*]' | tr -d '[:punct:]'| tr [:upper:] [:lower:] | grep -v "^\s*$" | sort | uniq -c | sort -bnr | awk -v a="$3" 'int($1)>=a {print $0}' > word_count/$1_${sub}.txt 
 	
 		
 done < comment_count/count_$1.txt
 
 
+#sed -i 's/^ *//' 
